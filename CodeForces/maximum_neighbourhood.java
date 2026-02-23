215b7251e757f3ea42e5c2ce18daf50be4965d2b
Fahim Akhter <fahim.nes.gamerz@gmail.com>
2026-02-23 16:11:32 +0530

    change

--- CodeForces/maximum_neighbourhood.java
+++ CodeForces/maximum_neighbourhood.java
@@ -8,10 +8,9 @@ public class maximum_neighbourhood
         while(tc-->0)
         {
             int n=in.nextInt();
-            System.out.println((n==1) ? n : (n==2) ? n : Math.max((4*n*n)-n-4 , (5*n*n)-5*n-5));
-            // if(n==1) System.out.println(n);
-            // if(n==2) System.out.println(9);
-            // if(n>2) System.out.println(Math.max((4*n*n)-n-4 , (5*n*n)-5*n-5));
+            if(n==1) System.out.println(n);
+            if(n==2) System.out.println(9);
+            if(n>2) System.out.println(Math.max((4*n*n)-n-4 , (5*n*n)-5*n-5));
         }
     }
 }
